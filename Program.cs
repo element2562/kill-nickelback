0c6e5f7cbea5b18b0c156829fa04050137b02fac
element2562 <element25668@gmail.com>
2018-08-27 11:36:55 -0500

    finished

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NickelBack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, string)> goodSongs = new List<(string, string)>();
+            HashSet<(string, string)> songs = new HashSet<(string, string)>();
+            songs.Add(("Nickelback", "Goodbye"));
+            songs.Add(("Slipknot", "Psychosocial"));
+            songs.Add(("Alice in Chains", "Rooster"));
+            songs.Add(("Nickelback", "Rockstar"));
+            songs.Add(("Soundgarden", "Outshined"));
+            songs.Add(("Audioslave", "Like a Stone"));
+            songs.Add(("Nickelback", "Animals"));
+            foreach ((string, string) item in songs)
+            {
+                if(item.Item1 != "Nickelback")
+                {
+                    goodSongs.Add((item.Item1, item.Item2));
+                }
+            }
+            foreach ((string, string) item in goodSongs)
+            {
+                Console.WriteLine($"{item.Item2} by {item.Item1}");
+            }
+        }
+    }
+}
